45b5d30b9f388559f7b63475cfbf31a47004ac28
Agustin Violante <violanteagustin@gmail.com>
2024-04-27 19:37:40 -0300

    hicimos la app de clima

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {WheaterApp} from './WheaterApp'
+import './styles/styles.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WheaterApp />
+  </React.StrictMode>,
+)
